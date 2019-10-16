127f1d0f39128c8e228a79715337eee21b5dd04f
Alejandro <asantoflaminio@itba.edu.ar>
2019-10-16 15:39:35 -0300

    newest properties for rent

---
+++ frontend/src/pages/tileData.js
@@ -0,0 +1,27 @@
+const tileData = [
+  {
+    img: '../resources/burger.jpg',
+    title: 'Burger 1',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'C:/Users/aleja/Documents/FINAL PAW POSTA/paw-2018b-10/frontend/src/resources/burger.jpg',
+    title: 'Burger 2',
+    author: 'director90',
+  },
+  {
+    img: 'C:/Users/aleja/Documents/FINAL PAW POSTA/paw-2018b-10/frontend/src/resources/burger.jpg',
+    title: 'Burger 3',
+    author: 'director90',
+  },
+  {
+    img: 'C:/Users/aleja/Documents/FINAL PAW POSTA/paw-2018b-10/frontend/src/resources/burger.jpg',
+    title: 'Burger 4',
+    author: 'director90',
+  },
+  
+];
+
+export default tileData;
