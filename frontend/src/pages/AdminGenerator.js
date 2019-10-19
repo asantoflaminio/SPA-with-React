b3908b3c02ef6c9942136890fbcef20e575f789c
arodriguez1415 <arodriguez@itba.edu,ar>
2019-10-19 08:44:20 -0300

    avances admin

--- frontend/src/pages/AdminGenerator.js
+++ frontend/src/pages/AdminGenerator.js
@@ -8,12 +8,54 @@ import * as yup from 'yup';
 import { Formik } from 'formik';
 import '../css/AdminGenerator.css';
 import AdminManagment from '../components/AdminManagment';
+import {getJSON} from '../util/function'
 
 class AdminGenerator extends React.Component {
+
+    constructor(props) {
+        super(props);
+         this.state = { 
+             provinces: [],
+        };
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+        const data = getJSON(event.target,1)
+        const jsonObject = JSON.parse(data);
+        axios({
+          method: 'post',
+          url: 'admin/province',
+          data: jsonObject
+        })
+        .then(function (response) {
+            alert(response.status)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+    }
+
+    componentDidMount(){
+        axios
+          .get('admin/getProvinces')
+          .then(({ data })=> {
+            this.setState({
+                provinces: data,
+            });
+            alert(JSON.stringify(data))
+          })
+          .catch((err)=> {})
+      }
+
     render(){
         const { t } = this.props;
         const schemaProvince = yup.object({
+            province: yup.string().required(t('errors.requiredField'))
+        });
+        const schemacity = yup.object({
             province: yup.string().required(t('errors.requiredField')),
+            city: yup.string().required(t('errors.requiredField'))
         });
 
         return(
@@ -22,38 +64,77 @@ class AdminGenerator extends React.Component {
                 <AdminManagment t={t}/>
                 <div className="polaroid data">
                     <div className="title-container">       
-                        <h3>asd</h3>  
+                        <h3>{t('admin.locationTitle')}</h3>  
                     </div>
-                    <div className="signup-list-item">
-                    <Formik validationSchema={schemaProvince}>{({
-                        values,
-                        handleChange,
-                        errors,
-                        }) => (
-                            <Form noValidate onSubmit={event => {
-                                this.handleFormSubmit(event);
-                            }}>
-                                <Form.Group as={Col} md="12" controlId="validationFormik01">
-                                    <Form.Label>{t('admin.province')}</Form.Label>
-                                    <Form.Control
-                                        as="input"
-                                        type="text"
-                                        name="province"
-                                        className="location-input"
-                                        placeholder={t('admin.provinceHolder')}
-                                        value={values.province}
-                                        onChange={handleChange}
-                                        isInvalid={!!errors.province}
-                                    />
+                    <fieldset className="signup-list-item">
+                        <legend className="legendTag">{t('admin.provinceLegend')}</legend>
+                        <Formik validationSchema={schemaProvince}>{({
+                            values,
+                            handleChange,
+                            errors,
+                            }) => (
+                                <Form noValidate className="form-inline" onSubmit={event => {
+                                    this.handleFormSubmit(event);
+                                }}>
+                                    <Form.Group controlId="validationFormik01">
+                                        <Form.Label className="location-label">{t('admin.province')}</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="province"
+                                            className="location-input"
+                                            placeholder={t('admin.provinceHolder')}
+                                            value={values.province}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.province}
+                                        />
+                                    <Button type="submit" className="signup-submit">{t('admin.create')}</Button>
                                     <Form.Control.Feedback type="invalid">
-                                        {errors.province}
+                                            {errors.province}
                                     </Form.Control.Feedback>
-                                </Form.Group>
-                                <Button type="submit" className="submitButton">{t('admin.create')}</Button>
-                            </Form>
-                                )}
+                                    </Form.Group>
+                                </Form>
+                                    )}
                         </Formik>
-                    </div>
+                    </fieldset>
+                    <fieldset className="signup-list-item">
+                        <legend className="legendTag">{t('admin.cityLegend')}</legend>
+                        <Formik validationSchema={schemacity}>{({
+                            values,
+                            handleChange,
+                            errors,
+                            }) => (
+                                <Form noValidate className="form-inline" onSubmit={event => {
+                                    this.handleFormSubmit(event);
+                                }}>
+                                    <Form.Group controlId="validationFormik02">
+                                        <Form.Label className="location-label">{t('admin.province')}</Form.Label>
+                                        <Form.Control
+                                            as="select"
+                                            type="text"
+                                            name="province"
+                                            className="location-select"
+                                            value={values.province}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.province}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId="validationFormik03">
+                                        <Form.Label className="location-label">{t('admin.city')}</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="city"
+                                            className="location-input"
+                                            placeholder={t('admin.cityHolder')}
+                                            value={values.city}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.city}
+                                        />
+                                    <Button type="submit" className="signup-submit">{t('admin.create')}</Button>
+                                    </Form.Group>
+                                </Form>
+                                    )}
+                        </Formik>
+                    </fieldset>
                 </div>
             </div>
         );
