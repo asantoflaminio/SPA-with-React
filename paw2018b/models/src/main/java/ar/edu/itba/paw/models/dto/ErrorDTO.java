c31d7e3cbbb9e86eb6f3b718ee571661d3062174
arodriguez1415 <arodriguez@itba.edu,ar>
2020-01-23 12:39:44 -0300

    errorDTO - part 1

--- paw2018b/models/src/main/java/ar/edu/itba/paw/models/dto/ErrorDTO.java
+++ paw2018b/models/src/main/java/ar/edu/itba/paw/models/dto/ErrorDTO.java
@@ -2,19 +2,10 @@ package ar.edu.itba.paw.models.dto;
 
 public class ErrorDTO {
 
-	private int status;
 	private String error;
 	private String details;
 	
 	public ErrorDTO() {}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
 
 	public String getError() {
 		return error;
