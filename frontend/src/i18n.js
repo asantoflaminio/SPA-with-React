48c3885f467f747aa7bb85da4d5933e58bc560a6
Rocío Della Sala <rdella@itba.edu.ar>
2020-01-31 20:13:10 -0300

    i18n debug false

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -33,7 +33,7 @@ i18n
   .init({
     resources,
     fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
-    debug: true,
+    debug: false,
     whitelist: availableLanguages,
     detection: detectionOptions,
 
